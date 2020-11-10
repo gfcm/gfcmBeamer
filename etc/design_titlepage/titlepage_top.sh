7278aca02056b356933cd4a812333c1010bf2dd9
Arni Magnusson <arni.magnusson@ices.dk>
2020-11-10 16:47:36 +0100

    Version 1.0

---
+++ etc/design_titlepage/titlepage_top.sh
@@ -0,0 +1,10 @@
+2pdf titlepage_top.odp
+
+pcrop titlepage_top.pdf 532:418 608:1088
+out titlepage_top
+
+# pcrop titlepage_top.pdf 532:418 608:674
+# out banner_fao
+
+# pcrop titlepage_top.pdf 532:728 608:1088
+# out banner_gfcm
